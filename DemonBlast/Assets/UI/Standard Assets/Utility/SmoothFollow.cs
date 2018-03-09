32b02ea6c0961c58c0de7d6c7eef9f48a486f899
AlexandrePedrosa <36965280+AlexandrePedrosa@users.noreply.github.com>
2018-03-09 23:47:40 +0100

    Correction du Tuto

--- DemonBlast/Assets/UI/Standard Assets/Utility/SmoothFollow.cs
+++ DemonBlast/Assets/UI/Standard Assets/Utility/SmoothFollow.cs
@@ -5,23 +5,26 @@ namespace UnityStandardAssets.Utility
 	public class SmoothFollow : MonoBehaviour
 	{
 
+		private Quaternion lastRot;
 		// The target we are following
-		[SerializeField]
-		private Transform target;
+		public Rigidbody target;
 		// The distance in the x-z plane to the target
-		[SerializeField]
-		private float distance = 10.0f;
+		public Transform lookat;
+		public float distance = 10.0f;
 		// the height we want the camera to be above the target
-		[SerializeField]
-		private float height = 5.0f;
 
-		[SerializeField]
-		private float rotationDamping;
-		[SerializeField]
-		private float heightDamping;
+		public float height = 5.0f;
+
+
+		public float rotationDamping;
+
+		public float heightDamping;
 
 		// Use this for initialization
-		void Start() { }
+		void Start()
+		{ 
+			lastRot = transform.rotation;
+		}
 
 		// Update is called once per frame
 		void LateUpdate()
@@ -31,10 +34,11 @@ namespace UnityStandardAssets.Utility
 				return;
 
 			// Calculate the current rotation angles
-			var wantedRotationAngle = target.eulerAngles.y;
+			var wantedRotationAngle = target.transform.rotation.y;
 			var wantedHeight = target.position.y + height;
 
-			var currentRotationAngle = transform.eulerAngles.y;
+			var currentRotationAngle = transform.rotation.y;
+
 			var currentHeight = transform.position.y;
 
 			// Damp the rotation around the y-axis
@@ -44,18 +48,20 @@ namespace UnityStandardAssets.Utility
 			currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 
 			// Convert the angle into a rotation
-			var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
 
 			// Set the position of the camera on the x-z plane to:
 			// distance meters behind the target
-			transform.position = target.position;
+			transform.position = lookat.position;
+			var currentRotation = Quaternion.Euler(-target.rotation.x + lastRot.x, -target.rotation.y + lastRot.y + currentRotationAngle,-target.rotation.z + lastRot.z);
 			transform.position -= currentRotation * Vector3.forward * distance;
 
 			// Set the height of the camera
 			transform.position = new Vector3(transform.position.x ,currentHeight , transform.position.z);
 
 			// Always look at the target
-			transform.LookAt(target);
+			transform.LookAt(lookat);
+			lastRot = target.transform.rotation;
 		}
 	}
 }
