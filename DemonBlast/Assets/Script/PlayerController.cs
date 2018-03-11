79cb205cbd88ba763bd50d2d020bba77147890f3
AlexandrePedrosa <36965280+AlexandrePedrosa@users.noreply.github.com>
2018-03-11 12:38:08 +0100

    j'ai merge camera et master à la demande de Robin

---
+++ DemonBlast/Assets/Script/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+
+
+	void Update()
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+	}
+
+	// This [Command] code is called on the Client …
+	// … but it is run on the Server!
+	public override void OnStartLocalPlayer ()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+}
