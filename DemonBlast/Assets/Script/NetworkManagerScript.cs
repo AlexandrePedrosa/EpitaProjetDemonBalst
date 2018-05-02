9c9769c26324b61afdd74bb88d2153a8b935dd5c
Tacitus701 <36673866+Tacitus701@users.noreply.github.com>
2018-05-02 23:32:15 +0200

    ajustement

--- DemonBlast/Assets/Script/NetworkManagerScript.cs
+++ DemonBlast/Assets/Script/NetworkManagerScript.cs
@@ -17,6 +17,8 @@ namespace UnityEngine.Networking
 		// Runtime variable
 		bool m_ShowServer;
 
+		private bool isPaused = false;
+
 		void Awake()
 		{
 			manager = GetComponent<NetworkManager>();
@@ -24,6 +26,16 @@ namespace UnityEngine.Networking
 
 		void Update()
 		{
+			if(Input.GetKeyDown(KeyCode.Escape))
+				isPaused = !isPaused;
+
+
+			if(isPaused)
+				Time.timeScale = 0f; // Le temps s'arrete
+
+			else
+				Time.timeScale = 1.0f; // Le temps reprend
+			
 			if (!showGUI)
 				return;
 
@@ -56,6 +68,27 @@ namespace UnityEngine.Networking
 
 		void OnGUI()
 		{
+			if(isPaused)
+			{
+
+				// Si le bouton est présser alors isPaused devient faux donc le jeu reprend.
+				if(GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 20, 80, 40), "Continuer"))
+				{
+					isPaused = false;
+				}
+
+				// Si le bouton est présser alors on ferme completement le jeu ou charge la scene "Menu Principal
+				// Dans le cas du bouton quitter il faut augmenter sa postion Y pour qu'il soit plus bas
+				if(GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 + 40, 80, 40), "Quitter"))
+				{
+					// Application.Quit(); 
+					manager.StopHost();
+					Application.LoadLevel("Menu"); 
+
+				}
+
+			}
+
 			if (!showGUI)
 				return;
 
